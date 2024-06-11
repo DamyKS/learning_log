2c2df105b2442ec0c8d413c513b41fccb7ebdf44
damyks <damianojeh2@gmail.com>
2024-06-11 14:46:12 +0100

    typo

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py migrate
-python world_champ_2022/manage.py createsuperuser --no-input
+python manage.py createsuperuser --no-input
