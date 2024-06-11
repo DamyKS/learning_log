22360685521a1e78177bd26127c73af6ea6595c7
damyks <damianojeh2@gmail.com>
2024-06-11 14:31:25 +0100

    added super user build command

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py migrate
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
